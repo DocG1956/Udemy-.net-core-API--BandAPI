70c8b8570849ae2380746209753a57915842c9bb
DocG1956 <garybales1029@yahoo.com>
2021-02-16 17:45:05 -0600

    AutoMapper Implementation

--- Controllers/BandsController.cs
+++ Controllers/BandsController.cs
@@ -1,9 +1,12 @@
 ﻿using BandAPI.Services;
+using BandAPI.Models;
+using BandAPI.Entities;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BandAPI.Helpers;
 
 namespace BandAPI.Controllers
 {
@@ -22,14 +25,44 @@ namespace BandAPI.Controllers
         public IActionResult GetBands()
         {
             var bandsFromRepo = _bandAlbumRepository.GetBands();
-            return new JsonResult(bandsFromRepo);
+            return Ok(bandsFromRepo);
+        }
+
+        [HttpGet("/bandsModel")]
+
+        //public IActionResult GetBandBands()
+
+        //The above line can be replaced with the line below which is the base class of IActionResult
+        //because List is the default return type for the base class
+        public ActionResult<IEnumerable<BandDto>> GetBandBands()
+        {
+            var bandsFromRepo = _bandAlbumRepository.GetBands();
+            var bandsDto = new List<BandDto>();
+
+            foreach (var band in bandsFromRepo)
+            {
+                bandsDto.Add(new BandDto()
+                {
+                    Id = band.Id,
+                    Name = band.Name,
+                    MainGenre = band.MainGenre,
+                    FoundedYearsAgo = $"{band.Founded.ToString("yyyy")} ({band.Founded.GetYearsAgo()} years ago)"
+                });
+            }
+
+            return Ok(bandsDto);
+
         }
 
         [HttpGet("{bandId}")]
         public IActionResult GetBand(Guid bandId)
         {
             var bandsFromRepo = _bandAlbumRepository.GetBand(bandId);
-            return new JsonResult(bandsFromRepo);
+
+            if (bandsFromRepo == null)
+                return NotFound();
+
+            return Ok(bandsFromRepo);
         }
     }
 }
---
+++ Profiles/Profiles.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using BandAPI.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandAPI.Profiles
+{
+    public class BandsProfile : Profile
+    {
+        public BandsProfile()
+        {
+            CreateMap<Entities.Band, Models.BandDto>()
+                .ForMember(
+                    dest => dest.FoundedYearsAgo,
+
+                    //single line implementation of string interpolation
+                    opt => opt.MapFrom(src => $"{src.Founded.ToString("yyyy") + (src.Founded.GetYearsAgo()) + " years ago"}"));
+                    
+                    // Double line implementation of string interpolation
+                    //opt => opt.MapFrom(src => $"{src.Founded.ToString("yyyy")} " +
+                    //$"({src.Founded.GetYearsAgo()}) years ago"));
+                    
+        }
+    }
+}
+  
